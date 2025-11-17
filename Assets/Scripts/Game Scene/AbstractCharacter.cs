8731e891bbea4bf1cef0001673314d2fc6c2f79c
volanre <113961831+volanre@users.noreply.github.com>
2025-11-17 14:35:09 -0800

    added home and game screens

---
+++ Assets/Scripts/Game Scene/AbstractCharacter.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public abstract class AbstractCharacter : MonoBehaviour
+{
+    [SerializeField] protected float speed;
+    [SerializeField] protected float health;
+    [SerializeField] protected float damage;
+    [SerializeField] protected float defense;
+
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
