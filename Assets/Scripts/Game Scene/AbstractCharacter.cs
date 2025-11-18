894e0c90ecb8325c01b171b8650b93cb7394dfeb
volanre <113961831+volanre@users.noreply.github.com>
2025-11-17 20:49:07 -0800

    implimented basic player controls

--- Assets/Scripts/Game Scene/AbstractCharacter.cs
+++ Assets/Scripts/Game Scene/AbstractCharacter.cs
@@ -1,10 +1,139 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public abstract class AbstractCharacter : MonoBehaviour
 {
-    [SerializeField] protected float speed;
-    [SerializeField] protected float health;
-    [SerializeField] protected float damage;
-    [SerializeField] protected float defense;
+    /* 
+    For all multipliers:
+        If bool is true then it is an MULTIPLICATIVE bonus
+        If bool is false then it is a ADDITIVE bonus
+    */
+    [NonSerialized] public List<Tuple<bool, float>> damageMultipliers = new List<Tuple<bool, float>>();
+    [NonSerialized] public List<Tuple<bool, float>> defenseMultipliers = new List<Tuple<bool, float>>();
+    [NonSerialized] public List<Tuple<bool, float>> speedMultipliers = new List<Tuple<bool, float>>();
+    [NonSerialized] public List<Tuple<bool, float>> fireRateMultipliers = new List<Tuple<bool, float>>();
+
+    [Header("Character Properties")]
+    [NonSerialized] public int health = 999999999;
+    [SerializeField] public string title;
+    [SerializeField] public int maxHealth = 500;
+    [SerializeField] public int attack = 100, defense = 100;
+    [SerializeField] public float speed = 5f;
+    [SerializeField] public float jumpForce = 10f;
+    [SerializeField] public float fireRate = 0.3f;
+
+    /// <summary>
+    /// Call this whenever the character takes damage
+    /// </summary>
+    public void TakeDamage(int attackedValue, bool ignoreDefense = false)
+    {
+        int effectiveDamage = ignoreDefense ? attackedValue : CalculateEffectiveDamage(attackedValue);
+        health -= effectiveDamage;
+        gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+        DamageEffects();
+        Invoke("ResetColor", 0.07f);
+    }
+
+    /// <summary>
+    /// Extra events that occur after taking damage
+    /// </summary>
+    public abstract void DamageEffects();
+    private void ResetColor()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    /// <summary>
+    /// Calculates how much damage is actually dealt to the character using a formula <br/>
+    /// This is NOT for calculating how much damage the character does to others
+    /// </summary>
+    /// <returns></returns>
+    public int CalculateEffectiveDamage(int attackedValue)
+    {
+        if (attackedValue <= 0) return 0;
+        float currentDamage = (attackedValue * attackedValue) / (attackedValue + GetEffectiveDefense());
+        currentDamage = (int)currentDamage;
+        currentDamage = currentDamage <= 0 ? 1 : currentDamage;
+        return (int)currentDamage;
+    }
+
+    /// <summary>
+    /// Gets the character's effective attack value.
+    /// </summary>
+    /// <returns></returns>
+    public int GetEffectiveAttack()
+    {
+        float newDamage = attack;
+        var multiplicativeOnly = damageMultipliers.Where(x => x.Item1 == true).ToList();
+        var additiveOnly = damageMultipliers.Where(x => x.Item1 == false).ToList();
+        foreach ((bool type, float value) in multiplicativeOnly)
+        {
+            newDamage = newDamage * value;
+        }
+        foreach ((bool type, float value) in additiveOnly)
+        {
+            newDamage += value;
+        }
+        return (int)newDamage;
+    }
+    /// <summary>
+    /// Gets the character's effective move speed.
+    /// </summary>
+    /// <returns></returns>
+    public float GetEffectiveSpeed()
+    {
+        float newSpeed = speed;
+        var multiplicativeOnly = speedMultipliers.Where(x => x.Item1 == true).ToList();
+        var additiveOnly = speedMultipliers.Where(x => x.Item1 == false).ToList();
+        foreach ((bool type, float value) in multiplicativeOnly)
+        {
+            newSpeed = newSpeed * value;
+        }
+        foreach ((bool type, float value) in additiveOnly)
+        {
+            newSpeed += value;
+        }
+        return newSpeed;
+    }
+    /// <summary>
+    /// Gets the character's effective firerate.
+    /// </summary>
+    /// <returns></returns>
+    public float GetEffectiveFireRate()
+    {
+        float newFire = fireRate;
+        var multiplicativeOnly = fireRateMultipliers.Where(x => x.Item1 == true).ToList();
+        var additiveOnly = fireRateMultipliers.Where(x => x.Item1 == false).ToList();
+        foreach ((bool type, float value) in multiplicativeOnly)
+        {
+            newFire = newFire * value;
+        }
+        foreach ((bool type, float value) in additiveOnly)
+        {
+            newFire += value;
+        }
+        return newFire;
+    }
+    /// <summary>
+    /// Gets the character's effective defense value.
+    /// </summary>
+    /// <returns></returns>
+    public int GetEffectiveDefense()
+    {
+        float newD = defense;
+        var multiplicativeOnly = defenseMultipliers.Where(x => x.Item1 == true).ToList();
+        var additiveOnly = defenseMultipliers.Where(x => x.Item1 == false).ToList();
+        foreach ((bool type, float value) in multiplicativeOnly)
+        {
+            newD = newD * value;
+        }
+        foreach ((bool type, float value) in additiveOnly)
+        {
+            newD += value;
+        }
+        return (int)newD;
+    }
 
 }
--- Assets/Scripts/Game Scene/Player.cs
+++ Assets/Scripts/Game Scene/Player.cs
@@ -1,16 +1,206 @@
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.ShaderGraph;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.UIElements;
 public class Player : AbstractCharacter
 {
+    private float attackTimer = 0f, dashTimer = 0f, jumpBoostTimer = 0f;
+    private Vector2 moveDirection = Vector2.zero, shootDirection = Vector2.zero, dashDirection = Vector2.left, attackDirection = Vector2.right;
+    private bool dashAvailable = true, isDashing = false, isJumping = false;
+    
+    [NonSerialized] public static bool isAlive = true;
+    private InputAction moveAction;
+    private InputAction attackAction;
+    private InputAction interactAction;
+    private InputAction dashAction;
+    private InputAction jumpAction;
+
+    [Header("References")]
+    // [SerializeField] public ProjectileBehavior projectileItem;
+    // [SerializeField] public SoundEffectPlayer noiseMaker;
+    [SerializeField] protected Rigidbody2D rb;
+    // [SerializeField] protected Animator animator;
+    // [SerializeField] public PlayerInputActions playerControls;
+    // [SerializeField] public HealthBarUI healthBar;
+    // [SerializeField] public HealthBarUI xpBar;
+    // [SerializeField] public HealthBarUI dashBar;
+
+    [Header("Extra Player Items")]
+    [SerializeField] public float dashCooldown = 0.27f;
+    [SerializeField] public float dashLength = 0.35f;
+    [SerializeField] private float jumpBoostForce = 20f;     // small continuous force
+    [SerializeField] private float maxJumpBoostTime = 0.15f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        health = maxHealth;
+
+        moveAction = InputSystem.actions.FindAction("Move");
+        moveAction.Enable();
+        jumpAction = InputSystem.actions.FindAction("Jump");
+        jumpAction.Enable();
+        dashAction = InputSystem.actions.FindAction("Dash");
+        dashAction.Enable();
+        attackAction = InputSystem.actions.FindAction("Attack");
+        attackAction.Enable();
+
+        moveAction.performed += MovePerformed;
+        moveAction.canceled += MoveCancelled;
+        dashAction.performed += DashPerformed;
+        dashAction.canceled += DashCancelled;
+        attackAction.performed += AttackPerformed;
+        attackAction.canceled += AttackCancelled;
+        jumpAction.performed += JumpPerformed;
+        jumpAction.canceled += JumpCancelled;
     }
 
-    // Update is called once per frame
+    private void OnDisable()
+    {
+        moveAction.Disable();
+        attackAction.Disable();
+        dashAction.Disable();
+        jumpAction.Disable();
+    }
+
     void Update()
     {
+        if (!isAlive) return;
+        UpdateStandardTimers();
+    }
+    private void FixedUpdate()
+    {
+        if (!isAlive) return;
+        UpdateFixedTimers();
+        if (!isDashing)
+        {
+            rb.linearVelocity = new Vector2(getVelocity().x, rb.linearVelocity.y);
+        }
+        if (isJumping && jumpBoostTimer < maxJumpBoostTime)
+        {
+            rb.AddForce(Vector2.up * jumpBoostForce * Time.fixedDeltaTime, ForceMode2D.Force);
+        }
+    }
+
+    public override void DamageEffects()
+    {
+        if (health <= 0)
+        {
+            Suicide();
+        }
+    }
+    private void Suicide()
+    {
+        rb.linearVelocity = Vector2.zero;
+        OnDisable();
+        // animator.SetBool("isDead", true);
+        if (isAlive)
+        {
+            // noiseMaker.PlayLongSound(deathSound, 1.2f);
+            var time = 500;
+            Invoke("CompleteDeath", time);
+        }
+        //Destroy(gameObject);
+    }
+    private void CompleteDeath()
+    {
+        isAlive = false;
+    }
+    public Vector2 getVelocity()
+    {
+        return new Vector2(moveDirection.x * GetEffectiveSpeed(), rb.linearVelocityY);
+    }
+    private void MovePerformed(InputAction.CallbackContext context)
+    {
+        moveDirection = context.ReadValue<Vector2>();
+        dashDirection = moveDirection;
+        Debug.Log("Moving here: " + moveDirection);
+        // animator.SetBool("isWalking", true);
+    }
+    private void MoveCancelled(InputAction.CallbackContext context)
+    {
+        moveDirection = Vector2.zero;
+        // animator.SetBool("isWalking", false);
+    }
+
+    private void DashPerformed(InputAction.CallbackContext context)
+    {
+        if (dashAvailable && (dashTimer > dashCooldown))
+        {
+            Vector2 attackInput = attackAction.ReadValue<Vector2>();
+            if (attackInput != Vector2.zero)
+            {
+                return; //no dashing while attacking
+            }
+
+            attackAction.Disable();
+            dashAvailable = false;
+            isDashing = true;
+            dashTimer = -100f;
+            float dashSpeed = speed * 4;
+            Vector2 dashVelocity = new Vector2(dashDirection.x * dashSpeed, 0);
+            rb.linearVelocityX = dashVelocity.x;
+            Invoke("EndDash", dashLength);
+
+            //animator.SetBool("isWalking", true);
+        }
+    }
+    private void EndDash()
+    {
+        attackAction.Enable();
+        dashTimer = 0f;
+        isDashing = false;
+        rb.linearVelocityX = getVelocity().x;
+    }
+    private void DashCancelled(InputAction.CallbackContext context)
+    {
+        dashAvailable = true;
+    }
+
+    private void AttackPerformed(InputAction.CallbackContext context)
+    {
+        if (attackTimer > GetEffectiveFireRate())
+        {
+            attackTimer = 0f;
+            attackDirection = moveDirection;
+            Debug.Log("Attacking!: " + attackDirection);
+        }
         
+    }
+    private void AttackCancelled(InputAction.CallbackContext context)
+    {
+        return;
+    }
+    private void JumpPerformed(InputAction.CallbackContext context)
+    {
+        if (Mathf.Abs(rb.linearVelocity.y) < 0.01f)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+            isJumping = true;
+            jumpBoostTimer = 0f;
+        }
+        
+    }
+    private void JumpCancelled(InputAction.CallbackContext context)
+    {
+        isJumping = false;
+        if (rb.linearVelocity.y > 0)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.33f);
+        }
+    }
+    
+    private void UpdateStandardTimers()
+    {
+        attackTimer += Time.deltaTime;
+        dashTimer += Time.deltaTime;
+    }
+    private void UpdateFixedTimers()
+    {
+        jumpBoostTimer += Time.fixedDeltaTime;
     }
 }
